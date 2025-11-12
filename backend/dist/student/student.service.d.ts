e35559ea8dae35d7efe6e5b957f310874743a19f
Hassan Osama <hassanusamacs@gmail.com>
2025-11-12 20:35:02 +0200

    added some code for the student module

---
+++ backend/dist/student/student.service.d.ts
@@ -0,0 +1,3 @@
+export declare class StudentService {
+    logHello(): void;
+}
--- backend/src/student/student.service.ts
+++ backend/src/student/student.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+
+    logHello(): void {
+        console.log("Hello from Student Service");
+    }
+}
